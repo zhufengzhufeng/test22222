b0dd3e796c9b0c3cb39a87b03759c76db7742f5c
zhufengzhufeng <894918097@qq.com>
2018-02-23 19:09:45 +0800

    ok

--- events.js
+++ events.js
@@ -1,7 +1,27 @@
 function EventEmitter(){
-    EventEmitter.init.call();
+    EventEmitter.init.call(this); // 初始化内部私有方法
 }
-
 EventEmitter.init = function(){
+    // 为了存放一对多的对应关系 例如后期 
+    // {'有钱',[buyPack,buyCar],'没钱':[hungry]}
     this._events = Object.create(Object.create(null));
 }
+EventEmitter.prototype.on = function(eventName,callback){ // 绑定事件
+    // 调用on方法就是维护内部的_events变量,使其生成一对多的关系
+    if(this._events[eventName]){ // 如果存在这样一个关系只需在增加一项即可
+        this._events[eventName].push(callback)
+    }else{
+        // 增加关系
+        this._events[eventName] = [callback]
+    }
+}
+EventEmitter.prototype.emit = function(eventName){ // 触发事件
+    if(this._events[eventName]){
+        // 如果有对应关系
+        this._events[eventName].forEach(callback => {
+            callback();
+        });
+    }
+}
+// 导出事件触发器类
+module.exports = EventEmitter; 
--- myEvents.js
+++ myEvents.js
@@ -1,10 +1,12 @@
-const EventEmitter = require('events');
+const EventEmitter = require('./events');
 class Man extends EventEmitter { }
 const man = new Man();
-man.on('有钱了', () => {
+let buyPack = () => {
     console.log('买包');
-});
-man.on('有钱了', () => {
+}
+let buyCar = () => {
     console.log('买车');
-});
+}
+man.on('有钱了', buyPack);
+man.on('有钱了', buyCar);
 man.emit('有钱了');
